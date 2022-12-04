5379c74469e70b5e4c32749cff06aca5377b6ac3
harlamovdv <d.v.harlamov@gmail.com>
2022-12-04 22:51:08 +0200

    HW_5

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    screens: {
+      sm: '320px',
+      md: '760px',
+      lg: '840px',
+      xl: '1350px',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
